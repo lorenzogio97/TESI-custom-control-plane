c65db52413cb3924c46c324cf278ca95ef62a38e
Lorenzo Giorgi <l.giorgi13@studenti.unipi.it>
2023-06-02 23:17:48 +0200

    Added methods to support Alt-Svc migration strategy and cloud Envoy Alt-Svc+Redirect route.

--- src/main/java/it/lorenzogiorgi/tesi/envoy/EnvoyConfigurationServer.java
+++ src/main/java/it/lorenzogiorgi/tesi/envoy/EnvoyConfigurationServer.java
@@ -1,7 +1,7 @@
 package it.lorenzogiorgi.tesi.envoy;
 
 import com.google.protobuf.Any;
-import com.google.protobuf.UInt32Value;
+import com.google.protobuf.Duration;
 import com.google.protobuf.util.Durations;
 import io.envoyproxy.controlplane.cache.v3.SimpleCache;
 import io.envoyproxy.controlplane.server.V3DiscoveryServer;
@@ -22,11 +22,11 @@ import io.envoyproxy.envoy.extensions.filters.network.http_connection_manager.v3
 import io.envoyproxy.envoy.extensions.transport_sockets.tls.v3.CommonTlsContext;
 import io.envoyproxy.envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext;
 import io.envoyproxy.envoy.extensions.transport_sockets.tls.v3.TlsCertificate;
-import io.envoyproxy.envoy.extensions.upstreams.http.v3.HttpProtocolOptions;
 import io.envoyproxy.envoy.type.matcher.v3.StringMatcher;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.netty.NettyServerBuilder;
+import it.lorenzogiorgi.tesi.common.CloudNode;
 import it.lorenzogiorgi.tesi.common.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -39,7 +39,7 @@ public class EnvoyConfigurationServer {
     private final Server server;
     private final ConcurrentHashMap<String, SnapshotInstance> proxiesSnapshot;
 
-    private static final Logger logger = LogManager.getLogger(EnvoyConfigurationServer.class.getName());
+    private final Logger logger = LogManager.getLogger(EnvoyConfigurationServer.class.getName());
     public EnvoyConfigurationServer() {
         proxiesSnapshot = new ConcurrentHashMap<>();
         // la lamda server per definire come ottenere il node-group dal node id,  in pratica è il criterio per
@@ -63,11 +63,12 @@ public class EnvoyConfigurationServer {
             throw new RuntimeException(e);
         }
 
+        //initialize proxy snapshots
         for(String edgeId: Configuration.edgeNodes.keySet()){
             proxiesSnapshot.put(edgeId, new SnapshotInstance());
-            addTLSListenerToProxy(edgeId, "default", "0.0.0.0");
-            addDefaultRouteToProxy(edgeId, "default");
         }
+        CloudNode cloudNode = Configuration.cloudNode;
+        proxiesSnapshot.put(cloudNode.getId(), new SnapshotInstance());
 
         logger.info("Envoy configuration server started successfully");
     }
@@ -80,10 +81,12 @@ public class EnvoyConfigurationServer {
             throw new RuntimeException(e);
         }
     }
+
     private void updateProxyCacheSnapshot(String proxyId) {
         globalCache.setSnapshot(proxyId, proxiesSnapshot.get(proxyId).getLastSnapshot());
     }
-    public boolean addListenerToProxy(String proxyId, String routeConfigName, String bindIPAddress, int bindPort) {
+
+    public void addListenerToProxy(String proxyId, String routeConfigName, String bindIPAddress, int bindPort) {
         logger.info("Add listener to Proxy: "+proxyId+" on port "+bindPort);
         ConfigSource.Builder configSourceBuilder = ConfigSource.newBuilder().setResourceApiVersion(ApiVersion.V3);
 
@@ -153,10 +156,8 @@ public class EnvoyConfigurationServer {
                 )
                 .build();
 
-        boolean result = proxiesSnapshot.get(proxyId).addResource(newListener);
-        if(!result) return false;
+        proxiesSnapshot.get(proxyId).addListener(newListener);
         updateProxyCacheSnapshot(proxyId);
-        return true;
 
     }
 
@@ -247,16 +248,15 @@ public class EnvoyConfigurationServer {
                 )
                 .build();
 
-        boolean result = proxiesSnapshot.get(proxyId).addResource(newListener);
-        if(!result) return;
+        proxiesSnapshot.get(proxyId).addListener(newListener);
         updateProxyCacheSnapshot(proxyId);
 
     }
 
-    public void addRouteToProxy(String proxyId, String username, String domain, String routeConfigName, String prefix,
-                                String userCookie, String destinationCluster) {
+    public void addUserRouteToProxy(String proxyId, String username, String domain, String routeConfigName, String prefix,
+                                    String userCookie, String destinationCluster) {
 
-        Route.Builder route = Route.newBuilder()
+        Route route = Route.newBuilder()
                 .setName(username+"-"+prefix)// sfruttare per l'eliminazione
                 .setMatch(
                         RouteMatch.newBuilder()
@@ -264,28 +264,42 @@ public class EnvoyConfigurationServer {
                                 .addHeaders(HeaderMatcher.newBuilder().setName("cookie").setStringMatch(StringMatcher.newBuilder().setContains("authID="+userCookie))))
                 .setRoute(
                         RouteAction.newBuilder()
+                                .setTimeout(Duration.newBuilder()
+                                        .setSeconds(5))
                                 .setPrefixRewrite("/")
-                                .setCluster(username+"-"+destinationCluster));
-
-
-        RouteConfiguration routeConfiguration = RouteConfiguration.newBuilder()
-                .setName(routeConfigName)
-                .addVirtualHosts(
-                        VirtualHost.newBuilder()
-                                .setName(Configuration.PLATFORM_DOMAIN)
-                                .addDomains("*."+Configuration.PLATFORM_DOMAIN)
-                                .addRoutes(route)
-                )
+                                .setCluster(username+"-"+destinationCluster))
                 .build();
 
-        boolean result = proxiesSnapshot.get(proxyId).addResource(routeConfiguration);
-        if(!result) return;
+
+        proxiesSnapshot.get(proxyId).addRoute(route);
         updateProxyCacheSnapshot(proxyId);
     }
 
-    public void addDefaultRouteToProxy(String proxyId, String routeConfigName) {
+    public void addPublicRouteToProxy(String proxyId, String prefix, String destinationCluster) {
 
-        Route.Builder route = Route.newBuilder()
+        Route route = Route.newBuilder()
+                .setName(destinationCluster+"-"+prefix)
+                .setMatch(
+                        RouteMatch.newBuilder()
+                                .setPathSeparatedPrefix(prefix)
+                )
+                .setRoute(
+                        RouteAction.newBuilder()
+                                .setTimeout(Duration.newBuilder()
+                                        .setSeconds(0))
+                                .setPrefixRewrite("/")
+                                .setCluster(destinationCluster)
+                )
+                .build();
+
+
+        proxiesSnapshot.get(proxyId).addRoute(route);
+        updateProxyCacheSnapshot(proxyId);
+    }
+
+    public void addDefaultRouteToProxy(String proxyId) {
+
+        Route route = Route.newBuilder()
                 .setName("default")
                 .setMatch(
                         RouteMatch.newBuilder()
@@ -293,24 +307,15 @@ public class EnvoyConfigurationServer {
                 )
                 .setDirectResponse(DirectResponseAction.newBuilder()
                         .setStatus(421)
-                );
-
-        RouteConfiguration routeConfiguration = RouteConfiguration.newBuilder()
-                .setName(routeConfigName)
-                .addVirtualHosts(
-                        VirtualHost.newBuilder()
-                                .setName(Configuration.PLATFORM_DOMAIN)
-                                .addDomains("*."+Configuration.PLATFORM_DOMAIN)
-                                .addRoutes(route)
                 )
                 .build();
 
-        boolean result = proxiesSnapshot.get(proxyId).addResource(routeConfiguration);
-        if(!result) return;
+
+        proxiesSnapshot.get(proxyId).addRoute(route);
         updateProxyCacheSnapshot(proxyId);
     }
 
-    public boolean addFeedbackRouteToProxy(String proxyId, String username, String domain, String routeConfigName, String prefix,
+    public void addFeedbackRouteToProxy(String proxyId, String username, String prefix,
                                    String userCookie, String destinationCluster) {
 
         LuaPerRoute lua= LuaPerRoute.newBuilder()
@@ -323,9 +328,9 @@ public class EnvoyConfigurationServer {
                                 "{" +
                                 "[\":method\"] = 'POST'," +
                                 "[\":path\"] = "+"'/migration_feedback/"+username+"/"+proxyId+"'," +
-                                "[\":authority\"] = 'orchestrator_cluster'" +
+                                "[\":authority\"] = '"+Configuration.PLATFORM_ORCHESTRATOR_DOMAIN+"'" +
                                 "}," +
-                                "\"Hello\", "+
+                                "\"\", "+
                                 "0,"+
                                 "true) " +
                                 "end"
@@ -333,7 +338,7 @@ public class EnvoyConfigurationServer {
                         .build())
                 .build();
 
-        Route.Builder route = Route.newBuilder()
+        Route route = Route.newBuilder()
                 .setName(username+"-"+prefix)
                 .setMatch(
                         RouteMatch.newBuilder()
@@ -343,57 +348,43 @@ public class EnvoyConfigurationServer {
                         RouteAction.newBuilder()
                                 .setPrefixRewrite("/")
                                 .setCluster(username+"-"+destinationCluster))
-                .putTypedPerFilterConfig("envoy.filters.http.lua", Any.pack(lua));
-
-        RouteConfiguration routeConfiguration =  RouteConfiguration.newBuilder()
-                .setName(routeConfigName)
-                .addVirtualHosts(
-                        VirtualHost.newBuilder()
-                                .setName(domain)
-                                .addDomains("*"+domain)
-                                .addRoutes(route)
-                )
+                .putTypedPerFilterConfig("envoy.filters.http.lua", Any.pack(lua))
                 .build();
 
-        boolean result = proxiesSnapshot.get(proxyId).addResource(routeConfiguration);
-        if(!result) return false;
+
+        proxiesSnapshot.get(proxyId).addRoute(route);
+
         updateProxyCacheSnapshot(proxyId);
-        return true;
     }
 
 
-    public boolean addRedirectToProxy(String proxyId, String username, String domain, String routeConfigName, String prefix,
-                                                 String userCookie, String destinationHost, int destinationPort) {
-        RouteConfiguration routeConfiguration = RouteConfiguration.newBuilder()
-                .setName(routeConfigName)
-                .addVirtualHosts(
-                        VirtualHost.newBuilder()
-                                .setName(domain)
-                                .addDomains(domain)
-                                .addRoutes(
-                                        Route.newBuilder()
-                                                .setDirectResponse(DirectResponseAction.newBuilder()
-                                                        .setStatus(308)
-                                                        .build())
-                                                .setName(username+"-"+prefix)
-                                                .setMatch(
-                                                        RouteMatch.newBuilder()
-                                                                .setPathSeparatedPrefix(prefix)
-                                                                .addHeaders(HeaderMatcher.newBuilder().setName("cookie").setStringMatch(StringMatcher.newBuilder().setContains("authID="+userCookie))))
-                                                .setRedirect(RedirectAction.newBuilder().setHostRedirect(destinationHost).setPortRedirect(destinationPort).setResponseCodeValue(308))
+    public void addAltSvcRedirectRouteToProxy(String proxyId, String username, String userCookie, String destinationEdgeHost) {
 
-                                )
+        Route route = Route.newBuilder()
+                .setName(username+"-user")
+                .setMatch(
+                        RouteMatch.newBuilder()
+                                .setPrefix("")
+                                .addHeaders(HeaderMatcher.newBuilder().setName("cookie").setStringMatch(StringMatcher.newBuilder().setContains("authID="+userCookie))))
+                .addResponseHeadersToAdd(HeaderValueOption.newBuilder()
+                        .setHeader(HeaderValue.newBuilder()
+                                .setKey("Alt-Svc")
+                                .setValue("h2=\""+destinationEdgeHost+":443\";")
+                                .build())
+                        .build())
+                .setRedirect(RedirectAction.newBuilder()
+                        .setHostRedirect(destinationEdgeHost)
+                        .setPortRedirect(443)
+                        .setResponseCodeValue(RedirectAction.RedirectResponseCode.TEMPORARY_REDIRECT_VALUE)
                 )
                 .build();
 
-        boolean result = proxiesSnapshot.get(proxyId).addResource(routeConfiguration);
-        if(!result) return false;
+        proxiesSnapshot.get(proxyId).addRoute(route);
         updateProxyCacheSnapshot(proxyId);
-        return true;
     }
 
 
-    public boolean addClusterToProxy(String proxyId, String username, String clusterName, String ip, int port) {
+    public void addClusterToProxy(String proxyId, String username, String clusterName, String ip, int port) {
 
         Cluster newCluster =  Cluster.newBuilder()
                 .setName(username+"-"+clusterName)
@@ -417,17 +408,18 @@ public class EnvoyConfigurationServer {
                                                                                                                 .setProtocolValue(SocketAddress.Protocol.TCP_VALUE)))))))
                 .build();
 
-        boolean result = proxiesSnapshot.get(proxyId).addResource(newCluster);
-        if(!result) return false;
+        proxiesSnapshot.get(proxyId).addCluster(newCluster);
         updateProxyCacheSnapshot(proxyId);
-        return true;
     }
 
-    public void convertRouteToRedirect(String username, String sourceProxyId, String destinationProxyId) {
-        SnapshotInstance snapshotInstance = proxiesSnapshot.get(sourceProxyId);
-        snapshotInstance.redirectProxyRoutesByUser(username, destinationProxyId);
-        //snapshotInstance.deleteClustersByUser(username);
+    public void convertRouteToMigratingByUserFromProxy(String username, String sourceProxyId, String destinationProxyId) {
+        proxiesSnapshot.get(sourceProxyId).convertRouteToMigratingByUser(username,destinationProxyId);
         updateProxyCacheSnapshot(sourceProxyId);
+    }
+
+    public void convertRouteToStableByUserFromProxy(String username, String proxyId) {
+        proxiesSnapshot.get(proxyId).convertRouteToStableByUser(username);
+        updateProxyCacheSnapshot(proxyId);
     }
 
     public void deleteAllUserResourcesFromProxy(String username, String proxyId) {
@@ -437,5 +429,19 @@ public class EnvoyConfigurationServer {
         updateProxyCacheSnapshot(proxyId);
     }
 
+    public void removeFeedbackFromRouteByUserFromProxy(String username, String proxyId) {
+        proxiesSnapshot.get(proxyId).convertRouteToStableByUser(username);
+        updateProxyCacheSnapshot(proxyId);
+    }
+
+    public void deleteClusterByNameFromProxy(String proxyId, String clusterName) {
+        proxiesSnapshot.get(proxyId).deleteClusterByName(clusterName);
+        updateProxyCacheSnapshot(proxyId);
+    }
+
+    public void deleteRouteByNameFromProxy(String proxyId, String routeName) {
+        proxiesSnapshot.get(proxyId).deleteRouteByName(routeName);
+        updateProxyCacheSnapshot(proxyId);
+    }
 
 }
